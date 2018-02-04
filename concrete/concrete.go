5fa315ef287e8ea7a1ad743e539148f05407c096
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-03 23:31:11 -0500

    Add documentation

--- concrete/concrete.go
+++ concrete/concrete.go
@@ -1,21 +1,27 @@
+// Package concrete uses the engine package
 package concrete
 
 import "github.com/miketmoore/test/engine"
 
+// Concrete is a struct that encapsulates an engine.Engine instance and an
+// additional name value
 type Concrete struct {
 	name   string
 	engine engine.Engine
 }
 
+// New returns an instance of Concrete with the specified values
 func New(id int, name string) Concrete {
 	e := engine.New(id)
 	return Concrete{name, e}
 }
 
+// Id is a getter for the internal engine.Engine ID value
 func (c Concrete) Id() int {
 	return c.engine.Id()
 }
 
+// Name is a getter for the name value
 func (c Concrete) Name() string {
 	return c.name
 }
--- engine/engine.go
+++ engine/engine.go
@@ -1,13 +1,17 @@
+// Package engine is the core, generic functionality that other packages should use
 package engine
 
+// Engine is a simple struct that encapsulates an ID
 type Engine struct {
 	id int
 }
 
+// Id is a getter for the internal ID value
 func (e Engine) Id() int {
 	return e.id
 }
 
+// New returns an instance of Engine, with the specified ID
 func New(id int) Engine {
 	return Engine{id}
 }
